dc76c1c069a83e8da76d13a095b03c3578571990
felipemarinhodev <felipmarinho@gmail.com>
2022-09-17 09:27:17 -0700

    a3.2 a3.3 - update Botao component

--- src/components/ABBotao/ABBotao.stories.tsx
+++ src/components/ABBotao/ABBotao.stories.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { ABBotao } from '.';
+import { ABBotao, BotaoProps } from '.';
 
 export default {
   title: 'Componentes/ABBotao',
+  args: {
+    children: 'Label',
+  },
   component: ABBotao,
 } as ComponentMeta<typeof ABBotao>;
 
-const Template: ComponentStory<typeof ABBotao> = () => <ABBotao />;
+const Template: ComponentStory<typeof ABBotao> = args => <ABBotao {...args} />;
 
 export const Primario = Template.bind({});
+
+export const Secundario = Template.bind({});
+
+Primario.args = {} as BotaoProps;
+
+Secundario.args = {
+  tipo: 'secundario',
+} as BotaoProps;
--- src/components/ABBotao/index.tsx
+++ src/components/ABBotao/index.tsx
@@ -1,23 +1,49 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const BotaoEstilizado = styled.button`
-  background: #eb9b00;
-  padding: 16px 32px;
-  border: 2px solid #eb9b00;
-  color: white;
-  font-size: 20px;
-  cursor: pointer;
-  &:hover {
-    background: #b87900;
-    border: 2px solid #b87900;
-  }
+const BotaoEstilizado = styled.button<BotaoProps>`
+  ${({ tipo }) => css`
+    ${colorsModifiers[tipo!]()}
+    padding: 16px 32px;
+    font-size: 20px;
+    cursor: pointer;
+  `}
 `;
 
-type BotaoProps = {
-  children: React.ReactNode;
+const colorsModifiers = {
+  primario: () => css`
+    background: #eb9b00;
+    border: 2px solid #eb9b00;
+    color: white;
+
+    &:hover {
+      background: #b87900;
+      border: 2px solid #b87900;
+    }
+  `,
+  secundario: () => css`
+    background: #fff;
+    color: #eb9b00;
+    border: 2px solid #eb9b00;
+
+    &:hover {
+      background: #fff;
+      border: 2px solid #b87900;
+      color: #b87900;
+    }
+  `,
 };
 
-export function ABBotao({ children }: BotaoProps) {
-  return <BotaoEstilizado>{children}</BotaoEstilizado>;
+export interface BotaoProps {
+  children: React.ReactNode;
+  tipo?: 'primario' | 'secundario';
+  onClick?: () => void;
+}
+
+export function ABBotao({ children, tipo = 'primario', onClick }: BotaoProps) {
+  return (
+    <BotaoEstilizado onClick={onClick} tipo={tipo}>
+      {children}
+    </BotaoEstilizado>
+  );
 }
